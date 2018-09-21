5209c15f7c1abd775a50013d27cb51fc5845682f
Mirco Bellagamba <mirco.bellag@gmail.com>
2018-09-21 15:01:27 +0200

    Add routing. Follow up

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,17 +9,11 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 }
 
